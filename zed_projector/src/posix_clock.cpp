2701028d1c12fcda7febfe11e7dc013459406de7
Gregory Meyer <gregjm@umich.edu>
2018-11-13 14:54:22 -0500

    add PosixClock

---
+++ zed_projector/src/posix_clock.cpp
@@ -0,0 +1,53 @@
+// BSD 3-Clause License
+//
+// Copyright (c) 2018, UMIGV
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+// * Redistributions of source code must retain the above copyright
+//   notice, this list of conditions and the following disclaimer.
+//
+// * Redistributions in binary form must reproduce the above copyright
+//   notice, this list of conditions and the following disclaimer in
+//   the documentation and/or other materials provided with the
+//   distribution.
+//
+// * Neither the name of the copyright holder nor the names of its
+//   contributors may be used to endorse or promote products derived
+//   from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// AS IS AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+#include <zed_projector/posix_clock.h>
+
+#include <time.h>
+
+namespace zp {
+
+PosixClock::time_point PosixClock::now() noexcept {
+	timespec tp;
+
+	// there's no way this can fail -- all POSIX implementations
+	// support CLOCK_REALTIME, tp is on the stack (in valid address
+	// space), and we're not calling clock_settime.
+	clock_gettime(CLOCK_REALTIME, &tp);
+
+	return time_point{ std::chrono::seconds{ tp.tv_sec }
+					   + std::chrono::nanoseconds{ tp.tv_nsec } };
+}
+
+} // namespace zp
